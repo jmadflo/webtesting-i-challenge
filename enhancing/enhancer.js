3fc8dc08621ae00022e7d64d8dbe9a9d2f1b659c
Juan Gerardo Madero Flores <juan@Juans-MacBook.attlocal.net>
2020-05-20 15:42:28 -0700

    repair function working and tested

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,22 +1,36 @@
+const flawedItemMessage = 'Your item has a flawed structure. Make sure it has name, durability, and enhancement properties'
 module.exports = {
   succeed,
   fail,
   repair,
   get,
-};
+  flawedItemMessage
+}
 
 function succeed(item) {
-  return { ...item };
+  
+  return { ...item }
 }
 
 function fail(item) {
-  return { ...item };
+  return { ...item }
 }
 
+// restores item durability to 100
 function repair(item) {
-  return { ...item };
+  if (EnsureValidItem(item)) {
+    return { ...item, durability: 100 }
+  } else {
+    return flawedItemMessage
+  }
+  
 }
 
 function get(item) {
-  return { ...item };
+  return { ...item }
+}
+
+// used typeof === number for durability and enhancement in case value is 0 which is a 'falsey' value
+function EnsureValidItem(item) {
+  return item && item.name && typeof(item.durability) === 'number' && typeof(item.enhancement) === 'number'
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,23 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require('./enhancer.js')
 // test away!
+describe('tests for enhancer functions', () => {
+    describe('tests for repair function', () => {
+        // makes sure we get error message if the item has an incorrect structure
+        it('returns error message if the item has wrong structure', () => {
+            const flawedItem = { bad: true }
+            expect(enhancer.repair(flawedItem)).toBe(enhancer.flawedItemMessage)
+        })
+        // makes sure durability is set to 100 after repair
+        it('returns an item object with 100 durability', () => {
+            const item = {
+                name: 'test-item',
+                durability: 10,
+                enhancement: 0
+            }
+            const returnedItem = enhancer.repair(item)
+            expect(returnedItem.durability).toBe(100)
+            expect(returnedItem.name).toBe('test-item')
+            expect(returnedItem.enhancement).toBe(0)
+        })
+    })
+})
